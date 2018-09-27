c2414bf3e1d35531c5de7680adb186ae2a524aa1
Eric Thibeault <quiksilver3491@gmail.com>
2018-09-27 14:06:45 -0400

    Finished hello world file

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+# Je suis un commentaire!
+# Ctrl+O = Sauvegarder sans quitter
+
+echo "Salut!"
+
+# exit
+# echo "on ne me verra jamais :("
+
+#-n = Ne pas faire de retour de de chariot
+echo -n "Vive "
+echo "Linux"
+
+echo -e "1\n2\\n3\n"
+
+read -p "Quel âge avez-vous : " age
+echo $age "ans! Vous paraissez plus jeune!"
+
+echo -n "Question : "
+read resultat
+echo $resultat
+
+
