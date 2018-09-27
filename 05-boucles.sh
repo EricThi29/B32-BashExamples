db737436914a1a998699258eb2db31d7bccbf68d
Eric Thibeault <quiksilver3491@gmail.com>
2018-09-27 14:55:34 -0400

    Finished boucles

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+list=`ls` # Accent grave (backtick)  permet d'exécuter une commande
+
+for element in $liste
+do
+	echo $element
+done
+
+resultat=o
+
+while test $resultat = o -o $resultat = a;do
+	read -p "Entrez une lettre : " resultat 
+done
+
+user=`whoami`
+echo "Salut $user"
+
+# alias patate="./-hello_world.sh"
+
